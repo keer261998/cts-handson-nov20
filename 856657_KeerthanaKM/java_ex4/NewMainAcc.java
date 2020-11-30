da738414c508abfb53e6d8064d54d8a47c5f8821
Keerthana K M <41839109+keer261998@users.noreply.github.com>
2020-11-30 16:52:25 +0530

    Update NewMainAcc.java

--- 856657_KeerthanaKM/java_ex4/NewMainAcc.java
+++ 856657_KeerthanaKM/java_ex4/NewMainAcc.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class NewMainAcc {
 	
-	public static void main(String[] args) throws AccountAlreadyException, AccountNotFoundException {
+	public static void main(String[] args) {
 		
 		Scanner s= new Scanner(System.in);
 		ServiceAcc sa=new ServiceAcc();
@@ -36,7 +36,12 @@ public class NewMainAcc {
 					acc.setName(name);
 					acc.setBalance(balance);
 					
-					sa.addAccount(acc);
+					try {
+						sa.addAccount(acc);
+					} catch (AccountAlreadyException e) {
+						// TODO Auto-generated catch block
+						e.printMessage();
+					}
 					break;
 				}
 				
@@ -44,7 +49,12 @@ public class NewMainAcc {
 					System.out.println("Enter the account number to be removed:");
 					int ano= s.nextInt();
 					
-					sa.removeAccounts(ano);
+					try {
+						sa.removeAccounts(ano);
+					} catch (AccountNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printMsg();
+					}
 					System.out.println("Removed account with account number "+ano);
 					break;
 				}
@@ -53,7 +63,12 @@ public class NewMainAcc {
 					System.out.println("Enter the account number to display the details:");
 					int ano= s.nextInt();
 					
-					sa.findAccountByNo(ano);
+					try {
+						sa.findAccountByNo(ano);
+					} catch (AccountNotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printMsg();
+					}
 					break;
 				}
 				
