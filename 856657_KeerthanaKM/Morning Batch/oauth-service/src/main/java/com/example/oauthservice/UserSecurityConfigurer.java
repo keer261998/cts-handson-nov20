1dfd019207c968d27f521c21863476ce6ceace41
Keerthana K M <41839109+keer261998@users.noreply.github.com>
2020-12-12 12:14:24 +0530

    Added OAUTH

---
+++ 856657_KeerthanaKM/Morning Batch/oauth-service/src/main/java/com/example/oauthservice/UserSecurityConfigurer.java
@@ -0,0 +1,35 @@
+package com.example.oauthservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+@Configuration
+public class UserSecurityConfigurer extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		.withUser("guest").password("{noop}guest123").roles("USER")
+		.and()
+		.withUser("bruce").password("{noop}bruce123").roles("ADMIN", "USER");
+	}
+	
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		
+		return super.authenticationManagerBean();
+	}
+	
+	@Bean
+	@Override
+	public UserDetailsService userDetailsServiceBean() throws Exception{
+		
+		return super.userDetailsServiceBean();
+	}
+
+}
